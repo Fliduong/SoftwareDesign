adf17b17a756c276beb3247813da9d2ad23f4a20
fiona <liduong@sheridancollege.ca>
2022-01-25 09:46:14 -0500

    add partTimeStudent class

---
+++ src/studentlist/partTimeStudent.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fionali
+ */
+public class partTimeStudent extends Student {
+    private int numCourses;
+    
+    public partTimeStudent (String name, in numCourses){
+        super(name);
+        this.numCourses = numCourses;
+    }
+    
+}
